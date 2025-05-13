cdc767981cddcf0defdfaf1b5e01cde43a7f7e2d
Baysuultan <suanbai.baisultan@narxoz.kz>
2025-05-14 03:09:20 +0500

    fixed problems

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,3 @@
-package maria;
-
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
--- src/Staticvalues.java
+++ src/Staticvalues.java
@@ -1,5 +1,3 @@
-package maria;
-
 import java.awt.Image;
 import java.io.File;
 import java.util.*;
